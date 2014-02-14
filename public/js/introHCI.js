21ed70a534f5c41e521421c307f73df5366765f4
Kennan Murphy-Sierra <kennan@stanford.edu>
2014-02-13 19:20:38 -0800

    disappearing stuff

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,7 +2,6 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	console.log("Page Ready");
 	initializePage();
 })
 
@@ -12,6 +11,10 @@ $(document).ready(function() {
 function initializePage() {
 	console.log("Page Ready");
 	// add any functionality and listeners you want here
+	$('#classfield').change(toggleUniReqButton);
+	$('#majorfield').change(toggleConcentrationField);
+	$('#textfield').change(toggleConcentrationButton);
+	
 	$(".test").click(function(){
 		console.log("Changing class");
 		$(".test").toggleClass("activeMajor");
@@ -36,13 +39,48 @@ function initializePage() {
 		var item = {}
 		item["classes"] = classes;
 		console.log(JSON.stringify(item));
-
-
 	});
-
 }
 
 function changeClass(e){
   console.log("Changing class");
   $(".test").toggleClass("activeMajor");
+}
+
+// Toggles visibility of Button under University Requirements
+function toggleUniReqButton() {
+	console.log("Reached toggleUniReqButton");
+	var year = document.getElementById('classfield').value;
+	console.log("year" + year);
+	if(year === "0") {
+		document.getElementById('classButton').style.visibility="hidden";
+	} else {
+		document.getElementById('classButton').style.visibility="visible";	
+	}
+}
+
+// Toggles visibility of Select Concentration Dropdown
+function toggleConcentrationField() {
+	console.log("Reached toggleConcentrationField");
+	var maj = document.getElementById('majorfield').value;
+	console.log("major" + maj);
+	if(maj === "0") {
+		document.getElementById('trackTitle').style.visibility="hidden";
+		document.getElementById('trackfield').style.visibility="hidden";
+	} else {
+		document.getElementById('trackTitle').style.visibility="visible";
+		document.getElementById('trackfield').style.visibility="visible";	
+	}
+}
+
+// Toggles visibility of Button under Major Requirements
+function toggleConcentrationButton() {
+	console.log("Reached toggleConcentrationButton");
+	var track = document.getElementById('trackfield').value;
+	console.log("track" + track);
+	if(track === "0") {
+		document.getElementById('majorButton').style.visibility="hidden";
+	} else {
+		document.getElementById('majorButton').style.visibility="visible";	
+	}
 }

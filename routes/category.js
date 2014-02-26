462288baabfab31e71c8b7ecd5aedea552c7fee7
eymyers <ethan.myers@gmail.com>
2014-02-26 02:23:18 -0800

    restyle and add details button

--- routes/category.js
+++ routes/category.js
@@ -6,17 +6,26 @@ exports.viewCategory = function(req,res){
   var uniYear = "2014";
   var category = req.params.category;
   var requirement = req.params.requirement;
-  var requirements = data['requirements'][requirement][uniYear];
+  var major = 'Computer Science';
+  if(requirement == "University"){
+      major = "Buffer";
+  }
+  console.log(major);
+  var requirements = data['requirements'][requirement][uniYear][major];
   //console.log(requirements);
   req.session.current_category = category;
   req.session.current_requirement = requirement;
 
   var classes_in_category;
+  var class_electives;
+  var about;
 
   for(var i=0; i< requirements.length; i++){
     var obj = requirements[i];
     if(obj.name == category){
       classes_in_category = obj.classes;
+      class_electives = obj.electives;
+      about = obj.about;
     }
   }
 
@@ -36,6 +45,8 @@ exports.viewCategory = function(req,res){
     "requirement" : requirement, 
     "category" : category, 
     "classes" : classes_in_category,
+    "electives" : class_electives,
+    "about" : about,
     "current_classes" : current_classes
   });
 }
